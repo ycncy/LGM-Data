ac7a28c13d4e2d5b00374289d8241f472aca6632
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-05-02 14:51:14 +0200

    Modification des appels API (jusqu'aux tournois) vers la version finale (sans les listes pas conventionnelles)

--- src/transformer.py
+++ src/transformer.py
@@ -1,89 +1,83 @@
 import pandas as pd
 
 
-def clean_videogame_dataframe(videogame_info):
-    dataframe, id_list = videogame_info
-
+def clean_videogames_dataframe(videogame_raw_df):
     # On récupère uniquement les colonnes de la base de données finale
-    keys_filter = dataframe.filter(items=["id", "name", "slug", "current_version"])
+    keys_filter = videogame_raw_df.filter(items=["id", "name", "slug", "current_version"])
 
     # Conversion des colonnes dans les types souhaités
     keys_filter["id"] = keys_filter["id"].astype(int)
     keys_filter[["slug", "name"]] = keys_filter[["slug", "name"]].astype(str)
 
+    keys_filter.set_index("id", inplace=True)
+    keys_filter.index = keys_filter.index.astype(int)
+
     # Suppression des lignes dupliquées
     cleaned_dataframe = keys_filter.drop_duplicates()
 
-    cleaned_dataframe.set_index("id", inplace=True)
-
-    return cleaned_dataframe, id_list
+    return cleaned_dataframe
 
 
-def clean_league_dataframe(league_info):
-    dataframe, id_list = league_info
-
+def clean_leagues_dataframe(leagues_raw_df):
     # On récupère ici uniquement les colonnes que l'on veut garder dans la table finale
-    keys_filter = dataframe.filter(items=["id", "image_url", "name", "videogame_id"])
+    keys_filter = leagues_raw_df.filter(items=["id", "image_url", "name", "videogame_id"])
 
     # Conversion des colonnes dans les types souhaités
-    keys_filter["id"] = keys_filter["id"].apply(pd.to_numeric)
+    keys_filter["id"] = keys_filter["id"].astype(int)
 
     keys_filter[["name", "image_url"]] = keys_filter[["name", "image_url"]].astype(str)
 
+    keys_filter.set_index("id", inplace=True)
+    keys_filter.index = keys_filter.index.astype(int)
+
     # Suppression des lignes dupliquées
     cleaned_dataframe = keys_filter.drop_duplicates()
 
-    cleaned_dataframe.set_index("id", inplace=True)
-
-    return cleaned_dataframe, id_list
+    return cleaned_dataframe
 
 
-def clean_serie_dataframe(serie_info):
-    dataframe, id_list = serie_info
-
+def clean_series_dataframe(series_raw_df):
     # On récupère ici uniquement les colonnes que l'on veut garder dans la table finale
-    keys_filter = dataframe.filter(items=["id", "league_id", "full_name", "slug", "begin_at", "end_at"])
+    keys_filter = series_raw_df.filter(items=["id", "league_id", "full_name", "slug", "begin_at", "end_at"])
 
     # Conversion des colonnes dans les types souhaités
-    keys_filter[["id", "league_id"]] = keys_filter[["id", "league_id"]].apply(pd.to_numeric)
+    keys_filter[["id", "league_id"]] = keys_filter[["id", "league_id"]].astype(int)
     keys_filter[["full_name", "slug"]] = keys_filter[["full_name", "slug"]].astype(str)
     keys_filter["begin_at"] = pd.to_datetime(keys_filter["begin_at"], format="%Y-%m-%dT%H:%M:%SZ")
     keys_filter["end_at"] = pd.to_datetime(keys_filter["end_at"], format="%Y-%m-%dT%H:%M:%SZ")
 
+    keys_filter.set_index("id", inplace=True)
+    keys_filter.index = keys_filter.index.astype(int)
+
     # Suppression des lignes dupliquées
     cleaned_dataframe = keys_filter.drop_duplicates()
 
-    cleaned_dataframe.set_index("id", inplace=True)
-
-    return cleaned_dataframe, id_list
+    return cleaned_dataframe
 
 
-def clean_tournament_dataframe(tournament_info):
-    dataframe, id_list = tournament_info
-
+def clean_tournaments_dataframe(tournaments_raw_df):
     # On récupère ici uniquement les colonnes que l'on veut garder dans la table finale
-    keys_filter = dataframe.filter(items=["id", "slug", "begin_at", "end_at", "name", "serie_id", "winner_id", "tier", "has_bracket", "prizepool"])
+    keys_filter = tournaments_raw_df.filter(items=["id", "slug", "begin_at", "end_at", "name", "serie_id", "winner_id", "tier", "has_bracket", "prizepool"])
 
     # Conversion des colonnes dans les types souhaités
-    keys_filter[["id", "serie_id", "winner_id"]] = keys_filter[["id", "serie_id", "winner_id"]].apply(pd.to_numeric)
+    keys_filter[["id", "serie_id", "winner_id"]] = keys_filter[["id", "serie_id", "winner_id"]].astype(int)
     keys_filter[["name", "slug", "tier"]] = keys_filter[["name", "slug", "tier"]].astype(str)
     keys_filter["has_bracket"] = keys_filter["has_bracket"].astype(bool)
     keys_filter["begin_at"] = pd.to_datetime(keys_filter["begin_at"], format="%Y-%m-%dT%H:%M:%SZ")
     keys_filter["end_at"] = pd.to_datetime(keys_filter["end_at"], format="%Y-%m-%dT%H:%M:%SZ")
 
+    keys_filter.set_index("id", inplace=True)
+    keys_filter.index = keys_filter.index.astype(int)
+
     # Suppression des lignes dupliquées
     cleaned_dataframe = keys_filter.drop_duplicates()
 
-    cleaned_dataframe.set_index("id", inplace=True)
-
-    return cleaned_dataframe, id_list
+    return cleaned_dataframe
 
 
-def clean_match_dataframe(match_info):
-    dataframe, id_list = match_info
-
+def clean_matches_dataframe(matches_raw_df):
     # On récupère ici uniquement les colonnes que l'on veut garder dans la table finale
-    keys_filter = dataframe.filter(items=["id", "name", "slug", "match_type", "number_of_games", "tournament_id", "status", "draw", "winner_id", "original_scheduled_at", "scheduled_at", "begin_at", "end_at"])
+    keys_filter = matches_raw_df.filter(items=["id", "name", "slug", "match_type", "number_of_games", "tournament_id", "status", "draw", "winner_id", "original_scheduled_at", "scheduled_at", "begin_at", "end_at", "games_id_list"])
 
     # Conversion des colonnes dans les types souhaités
     keys_filter[["id", "tournament_id", "winner_id", "number_of_games"]] = keys_filter[["id", "tournament_id", "winner_id", "number_of_games"]].apply(pd.to_numeric)
@@ -101,13 +95,16 @@ def clean_match_dataframe(match_info):
     cleaned_dataframe = keys_filter.drop_duplicates()
 
     cleaned_dataframe.set_index("id", inplace=True)
+    cleaned_dataframe.index = cleaned_dataframe.index.astype(int)
 
-    return cleaned_dataframe, id_list
+    return cleaned_dataframe
 
 
-def clean_games_dataframe(games_info):
+def clean_games_dataframe(games_raw_df):
     # On récupère ici uniquement les colonnes que l'on veut garder dans la table finale
-    keys_filter = games_info.filter(items=["id", "begin_at", "end_at", "match_id", "finished", "winner.id", "forfeit", "length", "complete"])
+    keys_filter = games_raw_df.filter(items=["id", "begin_at", "end_at", "match_id", "finished", "winner.id", "forfeit", "length", "complete"])
+
+    keys_filter.dropna(inplace=True, subset=["begin_at", "end_at"])
 
     # Conversion des colonnes dans les types souhaités
     keys_filter[["id", "match_id", "winner.id", "length"]] = keys_filter[["id", "match_id", "winner.id", "length"]].apply(pd.to_numeric)
@@ -121,32 +118,29 @@ def clean_games_dataframe(games_info):
     cleaned_dataframe = keys_filter.drop_duplicates()
 
     cleaned_dataframe.set_index("id", inplace=True)
+    cleaned_dataframe.index = cleaned_dataframe.index.astype(int)
 
     return cleaned_dataframe
 
 
-def clean_tournaments_participants_dataframe(tournament_info):
-    dataframe, id_list = tournament_info
+def clean_streams_dataframe(streams_raw_df):
+    # On récupère uniquement les colonnes de la base de données finale
+    keys_filter = streams_raw_df.drop("main", axis=1)
 
-    # On récupère ici uniquement les colonnes que l'on veut garder dans la table finale
-    keys_filter = dataframe.filter(items=["id", "expected_roster"])
-
-    # On récupère l'id du tournoi passé en paramètres et on créé un dictionnaire vide
-    tournament_id = keys_filter["id"][0]
-
-    # On créé une liste vide pour y ajouter les futurs valeurs du dataframe (seulement les valeurs !!!!!!!!!!)
-    new_dataframe_values = []
-
-    # On ajoute toutes les futurs valeurs (l'id du tournoi et l'id de l'équipe)
-    for roster in keys_filter["expected_roster"][0]:
-        new_row = [int(tournament_id), int(roster["team"]["id"])]
-        new_dataframe_values.append(new_row)
-
-    # On créé le dataframe avec les valeurs créées juste au dessus
-    dataframe = pd.DataFrame(new_dataframe_values, columns=["tournament_id", "team_id"])
+    keys_filter["match_id"] = keys_filter["match_id"].astype(int)
+    keys_filter["official"] = keys_filter["official"].astype(bool)
 
     # Suppression des lignes dupliquées
-    cleaned_dataframe = dataframe.drop_duplicates()
+    cleaned_dataframe = keys_filter.drop_duplicates()
+
+    return cleaned_dataframe
+
+
+def clean_opponents_dataframe(opponents_raw_df):
+    opponents_raw_df[["match_id", "home_id", "away_id"]] = opponents_raw_df[["match_id", "home_id", "away_id"]].fillna(-1).astype(int)
+
+    # Suppression des lignes dupliquées
+    cleaned_dataframe = opponents_raw_df.drop_duplicates()
 
     return cleaned_dataframe
 
@@ -163,6 +157,10 @@ def clean_players_dataframe(players_dataframe):
     # Suppression des lignes dupliquées
     cleaned_dataframe = keys_filter_players_dataframe.drop_duplicates()
 
+
+    cleaned_dataframe.set_index("id", inplace=True)
+    cleaned_dataframe.index = cleaned_dataframe.index.astype(int)
+
     return cleaned_dataframe
 
 
@@ -178,31 +176,6 @@ def clean_team_dataframe(team_dataframe):
     cleaned_dataframe = keys_filter.drop_duplicates()
 
     cleaned_dataframe.set_index("id", inplace=True)
-
-    return cleaned_dataframe
-
-
-def clean_streams_dataframe(match_dataframe):
-    # On récupère uniquement les colonnes de la base de données finale
-    keys_filter = match_dataframe.drop("main", axis=1)
-
-    keys_filter["match_id"] = keys_filter["match_id"].astype(int)
-    keys_filter["official"] = keys_filter["official"].astype(bool)
-
-    # Suppression des lignes dupliquées
-    cleaned_dataframe = keys_filter.drop_duplicates()
-
-    return cleaned_dataframe
-
-
-def clean_opponents_dataframe(match_dataframe):
-    # On récupère uniquement les colonnes de la base de données finale
-    keys_filter = match_dataframe.drop("main", axis=1)
-
-    keys_filter["match_id"] = keys_filter["match_id"].astype(int)
-    keys_filter["official"] = keys_filter["official"].astype(bool)
-
-    # Suppression des lignes dupliquées
-    cleaned_dataframe = keys_filter.drop_duplicates()
+    cleaned_dataframe.index = cleaned_dataframe.index.astype(int)
 
     return cleaned_dataframe
