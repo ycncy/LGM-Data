d881255e5557b54af3f675b37e813f227abed3e8
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-05-13 00:44:17 +0200

    Maintenant il tourne toutes les heures

--- src/add_data_until_last_record.py
+++ src/add_data_until_last_record.py
@@ -1,5 +1,7 @@
 import concurrent.futures
 import datetime
+import schedule
+import time
 
 from pipeline.extract.extract_data_with_date_range import DateRangeDataExtractor
 from pipeline.transform.clean_and_transform_dataframe import *
@@ -10,33 +12,42 @@ date_range_data_extractor = DateRangeDataExtractor()
 mysql_data_manager = MySQLDataManager("lgm.cihggjssark1.eu-west-3.rds.amazonaws.com", "admin", "azertyuiop", "main")
 mysql_data_manager.connect_to_database()
 
-current_datetime = datetime.datetime.now()
-current_datetime = pd.to_datetime(current_datetime).tz_localize('UTC')
-
 videogames_id_list = [26, 1]
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    league_last_record_datetime = mysql_data_manager.get_last_record_datetime_from_table("league")
-    leagues_thread = executor.submit(date_range_data_extractor.fetch_leagues_with_date_range, league_last_record_datetime, current_datetime, videogames_id_list)
 
-    series_last_record_datetime = mysql_data_manager.get_last_record_datetime_from_table("serie")
-    series_thread = executor.submit(date_range_data_extractor.fetch_series_with_date_range, series_last_record_datetime, current_datetime, videogames_id_list)
+def run_program():
+    current_datetime = datetime.datetime.now()
+    current_datetime = pd.to_datetime(current_datetime).tz_localize('UTC')
 
-    tournaments_last_record_datetime = mysql_data_manager.get_last_record_datetime_from_table("tournament")
-    tournaments_thread = executor.submit(date_range_data_extractor.fetch_tournaments_with_date_range, tournaments_last_record_datetime, current_datetime, videogames_id_list)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        league_last_record_datetime = mysql_data_manager.get_last_record_datetime_from_table("league")
+        leagues_thread = executor.submit(date_range_data_extractor.fetch_leagues_with_date_range, league_last_record_datetime, current_datetime, videogames_id_list)
 
-    leagues_df = leagues_thread.result()
-    series_df = series_thread.result()
-    tournaments_df = tournaments_thread.result()
+        series_last_record_datetime = mysql_data_manager.get_last_record_datetime_from_table("serie")
+        series_thread = executor.submit(date_range_data_extractor.fetch_series_with_date_range, series_last_record_datetime, current_datetime, videogames_id_list)
 
-matches_df, matches_streams_df, matches_games_df, matches_opponents_df = date_range_data_extractor.fetch_raw_all_matches_infos(tournaments_df.id.to_list())
+        tournaments_last_record_datetime = mysql_data_manager.get_last_record_datetime_from_table("tournament")
+        tournaments_thread = executor.submit(date_range_data_extractor.fetch_tournaments_with_date_range, tournaments_last_record_datetime, current_datetime, videogames_id_list)
 
-mysql_data_manager.insert_into_table("league", leagues_df)
-mysql_data_manager.insert_into_table("serie", leagues_df)
-mysql_data_manager.insert_into_table("match", leagues_df)
-mysql_data_manager.insert_into_table("tournament", leagues_df)
-mysql_data_manager.insert_into_table("match_game", leagues_df)
-mysql_data_manager.insert_into_table("match_opponent", leagues_df)
-mysql_data_manager.insert_into_table("match_stream", leagues_df)
+        leagues_df = leagues_thread.result()
+        series_df = series_thread.result()
+        tournaments_df = tournaments_thread.result()
 
-mysql_data_manager.close_connection()
+    matches_df, matches_streams_df, matches_games_df, matches_opponents_df = date_range_data_extractor.fetch_raw_all_matches_infos(tournaments_df.id.to_list())
+
+    mysql_data_manager.insert_into_table("league", leagues_df)
+    mysql_data_manager.insert_into_table("serie", series_df)
+    mysql_data_manager.insert_into_table("match", matches_df)
+    mysql_data_manager.insert_into_table("tournament", tournaments_df)
+    mysql_data_manager.insert_into_table("match_game", matches_games_df)
+    mysql_data_manager.insert_into_table("match_opponent", matches_opponents_df)
+    mysql_data_manager.insert_into_table("match_stream", matches_streams_df)
+
+    mysql_data_manager.close_connection()
+
+
+schedule.every().hour.do(run_program)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
