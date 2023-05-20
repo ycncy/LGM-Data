d7e0d64d73ea5ab117581deed7d381e270f0aeae
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-05-20 17:14:38 +0200

    Changement de la méthode pour fetch les series avec des appels asynchrone

--- pipeline/extract/extract_data_with_date_range.py
+++ pipeline/extract/extract_data_with_date_range.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import requests
 
+import aiohttp
+import asyncio
+
 from pipeline.extract.data_extractor import DataExtractor
 
 
@@ -32,61 +35,59 @@ class DateRangeDataExtractor(DataExtractor):
 
         return leagues_df
 
-    def fetch_series_with_date_range(self, leagues_id_list, last_update_datetime):
+    async def fetch_league(self, session, league_id, last_update_datetime):
+        url = f"{self.api_url}/leagues/{league_id}/series?sort=-modified_at&page=1&per_page=100"
+        async with session.get(url) as response:
+            if response.status == 200:
+                response = await response.json()
+                first_dataframe = pd.json_normalize(response)
+                if not first_dataframe.empty:
+                    if "begin_at" in first_dataframe.columns:
+                        first_dataframe["begin_at"] = pd.to_datetime(first_dataframe["begin_at"])
+                        first_dataframe["end_at"] = pd.to_datetime(first_dataframe["end_at"])
+
+                        date_filtered_dataframe = first_dataframe[((first_dataframe['begin_at'].dt.year >= 2019) | (first_dataframe['end_at'].dt.year >= 2019)) & (first_dataframe.modified_at > last_update_datetime)]
+
+                        date_filtered_dataframe = date_filtered_dataframe.copy().reset_index(drop=False)
+                        date_filtered_dataframe.insert(len(date_filtered_dataframe.columns), "videogame_id", response[0]["videogame"]["id"])
+                        return date_filtered_dataframe
+
+    async def fetch_series_with_date_range(self, leagues_id_list, last_update_datetime):
         series_df = pd.DataFrame()
-
-        for league_id in leagues_id_list:
-            self.check_api_key()
-            url = f"{self.api_url}/leagues/{league_id}/series?sort=-modified_at&page=1&per_page=100"
-
-            response = requests.get(url, headers=self.header).json()
-
-            first_dataframe = pd.json_normalize(response)
-
-            if not first_dataframe.empty:
-                if "begin_at" in first_dataframe.columns:
-                    first_dataframe["begin_at"] = pd.to_datetime(first_dataframe["begin_at"])
-                    first_dataframe["end_at"] = pd.to_datetime(first_dataframe["end_at"])
-
-                    date_filtered_dataframe = first_dataframe[((first_dataframe['begin_at'].dt.year >= 2019) | (first_dataframe['end_at'].dt.year >= 2019)) & (first_dataframe.modified_at > last_update_datetime)]
-
-                    date_filtered_dataframe = date_filtered_dataframe.copy().reset_index(drop=False)
-                    date_filtered_dataframe.insert(len(date_filtered_dataframe.columns), "videogame_id", response[0]["videogame"]["id"])
-
-                    series_df = pd.concat([series_df, date_filtered_dataframe])
-
-                    self.api_call_counter += 1
-
+        tasks = []
+        async with aiohttp.ClientSession(headers=self.header) as session:
+            for league_id in leagues_id_list:
+                self.check_api_key()
+                task = self.fetch_league(session, league_id, last_update_datetime)
+                self.api_call_counter += 1
+                tasks.append(task)
+            results = await asyncio.gather(*tasks)
+            for result in results:
+                if result is not None:
+                    series_df = pd.concat([series_df, result])
         return series_df
 
     def fetch_tournaments_with_date_range(self, series_id_list, last_update_datetime):
         tournaments_df = pd.DataFrame()
 
-        for serie_id in series_id_list:
+        for series_id in series_id_list:
             self.check_api_key()
-            url = f"{self.api_url}/series/{serie_id}/tournaments?sort=-modified_at&page=1&per_page=100"
-
+            url = f"{self.api_url}/series/{series_id}/tournaments?sort=-modified_at&page=1&per_page=100"
             response = requests.get(url, headers=self.header).json()
-
             first_dataframe = pd.json_normalize(response)
 
-            if not first_dataframe.empty:
-                if "begin_at" in first_dataframe.columns:
-                    first_dataframe["begin_at"] = pd.to_datetime(first_dataframe["begin_at"])
-                    first_dataframe["end_at"] = pd.to_datetime(first_dataframe["end_at"])
-                    first_dataframe["modified_at"] = pd.to_datetime(first_dataframe["modified_at"])
+            if not first_dataframe.empty and "begin_at" in first_dataframe.columns:
+                first_dataframe["begin_at"] = pd.to_datetime(first_dataframe["begin_at"])
+                first_dataframe["end_at"] = pd.to_datetime(first_dataframe["end_at"])
+                first_dataframe["modified_at"] = pd.to_datetime(first_dataframe["modified_at"])
 
-                    date_filtered_dataframe = first_dataframe[((first_dataframe['begin_at'].dt.year >= 2019) | (first_dataframe['end_at'].dt.year >= 2019)) & (first_dataframe.modified_at > last_update_datetime)]
+                date_filtered_dataframe = first_dataframe[((first_dataframe['begin_at'].dt.year >= 2019) | (first_dataframe['end_at'].dt.year >= 2019)) & (first_dataframe.modified_at > last_update_datetime)]
+                date_filtered_dataframe["videogame_id"] = response[0]["videogame"]["id"]
 
-                    date_filtered_dataframe = date_filtered_dataframe.copy().reset_index(drop=False)
-                    date_filtered_dataframe.insert(len(date_filtered_dataframe.columns), "videogame_id", response[0]["videogame"]["id"])
-
-                    tournaments_df = pd.concat([tournaments_df, date_filtered_dataframe])
-
-                    self.api_call_counter += 1
+                tournaments_df = pd.concat([tournaments_df, date_filtered_dataframe])
+                self.api_call_counter += 1
 
         return tournaments_df
-
 
     def fetch_raw_all_matches_infos_with_date_range(self, tournaments_id_list, last_update_datetime):
         matches_raw_df = pd.DataFrame()
@@ -97,36 +98,34 @@ class DateRangeDataExtractor(DataExtractor):
         for tournament_id in tournaments_id_list:
             self.check_api_key()
             url = f"{self.api_url}/tournaments/{tournament_id}/matches"
-
             matches_info = requests.get(url, headers=self.header).json()
-
             first_dataframe = pd.json_normalize(matches_info)
 
-            date_filtered_dataframe = first_dataframe[((first_dataframe['begin_at'].dt.year >= 2019) | (first_dataframe['end_at'].dt.year >= 2019)) & (first_dataframe.modified_at > last_update_datetime)]
+            if not first_dataframe.empty and "begin_at" in first_dataframe.columns:
+                first_dataframe["begin_at"] = pd.to_datetime(first_dataframe["begin_at"])
+                first_dataframe["end_at"] = pd.to_datetime(first_dataframe["end_at"])
+                first_dataframe["modified_at"] = pd.to_datetime(first_dataframe["modified_at"])
 
-            matches_raw_df = pd.concat([matches_raw_df, date_filtered_dataframe])
+                date_filtered_dataframe = first_dataframe[((first_dataframe['begin_at'].dt.year >= 2019) | (first_dataframe['end_at'].dt.year >= 2019)) & (first_dataframe['modified_at'] > last_update_datetime)]
+                matches_raw_df = pd.concat([matches_raw_df, date_filtered_dataframe])
 
-            for match_info in matches_info:
-                if "games" in match_info:
-                    matches_games_raw_df = pd.concat([matches_games_raw_df, pd.json_normalize(match_info["games"])])
+                for match_info in matches_info:
+                    if "games" in match_info:
+                        matches_games_raw_df = pd.concat([matches_games_raw_df, pd.json_normalize(match_info["games"])])
 
-                if "streams_list" in match_info:
-                    streams_df = pd.json_normalize(match_info["streams_list"])
-                    streams_df["match_id"] = match_info["id"]
+                    if "streams_list" in match_info:
+                        streams_df = pd.json_normalize(match_info["streams_list"])
+                        streams_df["match_id"] = match_info["id"]
+                        matches_streams_raw_df = pd.concat([matches_streams_raw_df, streams_df])
 
-                    matches_streams_raw_df = pd.concat([matches_streams_raw_df, streams_df])
-
-                if "opponents" in match_info and isinstance(match_info["opponents"], list) and len(match_info["opponents"]) >= 2:
-                    opponents_dict = [{"home_id": match_info["opponents"][0]["opponent"]["id"], "away_id": match_info["opponents"][1]["opponent"]["id"], "match_id": match_info["id"]}]
-
-                    opponents_df = pd.DataFrame(opponents_dict)
-
-                    matches_opponents_raw_df = pd.concat([matches_opponents_raw_df, opponents_df])
+                    if "opponents" in match_info and isinstance(match_info["opponents"], list) and len(match_info["opponents"]) >= 2:
+                        opponents_dict = [{"home_id": match_info["opponents"][0]["opponent"]["id"], "away_id": match_info["opponents"][1]["opponent"]["id"], "match_id": match_info["id"]}]
+                        opponents_df = pd.DataFrame(opponents_dict)
+                        matches_opponents_raw_df = pd.concat([matches_opponents_raw_df, opponents_df])
 
             self.api_call_counter += 1
 
         return matches_raw_df, matches_streams_raw_df, matches_games_raw_df, matches_opponents_raw_df
-
 
     def fetch_raw_teams_and_players_from_tournaments_id_list(self, tournaments_id_list):
         teams_raw_df = pd.DataFrame()
