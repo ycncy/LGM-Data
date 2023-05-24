0bd30dbbfe805a6fffd26efabc94504d00509619
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-05-24 11:15:52 +0200

    :wrench: modification du fichier test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import sys
 
@@ -6,20 +7,22 @@ sys.path.append(parent_dir)
 
 from pipeline.load.mysql_data_manager import MySQLDataManager
 
+async def main():
+    try:
+        database_host = os.environ['DATABASE_HOST']
+        database_name = os.environ['DATABASE_NAME']
+        database_user = os.environ['DATABASE_USER']
+        database_password = os.environ['DATABASE_PASSWORD']
 
-try:
-    database_host = os.environ['DATABASE_HOST']
-    database_name = os.environ['DATABASE_NAME']
-    database_user = os.environ['DATABASE_USER']
-    database_password = os.environ['DATABASE_PASSWORD']
+        print(database_host)
+        print(database_name)
+        print(database_user)
+        print(database_password)
 
-    print(database_host)
-    print(database_name)
-    print(database_user)
-    print(database_password)
+        mysql_manager = MySQLDataManager(database_host, database_user, database_password, database_name)
+        await mysql_manager.connect_to_database()
 
-    mysql_manager = MySQLDataManager(database_host, database_user, database_password, database_name)
-    mysql_manager.connect_to_database()
+    except:
+        print("Error while connecting to database")
 
-except:
-    print("Error while connecting to database")
+asyncio.run(main())
