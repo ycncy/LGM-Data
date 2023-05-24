9302dc16f0041e71586067a2b504ffab3c9c8c37
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-05-24 10:36:27 +0200

    :wrench: modification du fichier test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'pipeline')))
 sys.path.extend(['..\\..\\Data'])
 from pipeline.load.mysql_data_manager import MySQLDataManager
 
