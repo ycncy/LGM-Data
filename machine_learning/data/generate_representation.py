adc7ec9623dc49de5940da181032bd71678ef029
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-05-29 23:50:02 +0200

    :recycle: Ajout de la fonction simple de génération du dataframe pour warren

--- machine_learning/data/generate_representation.py
+++ machine_learning/data/generate_representation.py
@@ -13,21 +13,12 @@ def clean_matches_infos_dataframe(matches_infos_dataframe):
 
 
 def generate_data_representations(matches_infos_dataframe):
-    matches_infos_dataframe = pd.read_pickle("../machine_learning/dataframes/data")
-
     dataframe_home_team = matches_infos_dataframe.rename({"away_id": "opponent_id", "home_id": "team_id"}, axis=1)
     dataframe_away_team = matches_infos_dataframe.rename({"home_id": "opponent_id", "away_id": "team_id"}, axis=1)
 
     concatenated_matches_infos_dataframe = pd.concat([dataframe_home_team, dataframe_away_team], ignore_index=True)
 
-    team_won_games = concatenated_matches_infos_dataframe[concatenated_matches_infos_dataframe["team_id"] == concatenated_matches_infos_dataframe["winner_id"]]
-    team_won_games = team_won_games[team_won_games["number_of_games"] > 1]
-
-    percentage_of_win_per_team_after_won_game_1 = team_won_games[team_won_games["winner_id"] == team_won_games["Game 1 winner_id"]].groupby("team_id").size() / team_won_games.groupby("team_id").size() * 100
-
-    percentage_of_win_per_team_after_won_game_1.name = "percentage_of_win_per_team_after_won_game_1"
-
-    team_won_games = pd.merge(team_won_games, percentage_of_win_per_team_after_won_game_1.to_frame(), left_on="team_id", right_index=True, how="left")
+    return concatenated_matches_infos_dataframe
 
     # nparray_home_team = np.array2string(dataframe_home_team.to_numpy(), separator=', ')
     # nparray_away_team = np.array2string(dataframe_away_team.to_numpy(), separator=', ')
