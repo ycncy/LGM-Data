e0ae7157e8e98dcdc634860425e62f8a1b21108a
Yacine Talhaoui <yacinetalhaoui13@gmail.com>
2023-06-03 20:20:02 +0200

    :art: Restructuration

--- pipeline/api_config.py
+++
@@ -1,6 +0,0 @@
-# Informations d'API
-API_URL = "https://api.pandascore.co"
-MDP = "bn1UUXXO5NcyJRECr8u2xA"
-
-
-HEADER = {"accept": "application/json", "authorization": "Bearer mnYh70jlLSFNbN8oyhNyJhCxNIPTkeE0T8LiS4A7tj6M-XjCYH0"}
